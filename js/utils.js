a0aa7701a9699872affc8733fc7ca659674dedcd
Andrès <andres.franco@etu.univ-nantes.fr>
2015-11-12 12:44:47 +0100

    Added utility (copy,override of objects) to utils

--- js/utils.js
+++ js/utils.js
@@ -1,26 +1,52 @@
-export function override_object(object,objectToOverride){
-    for (let key in object){
-        if (typeof object === "object" ){
-            deep_copy(object[key],objectToOverride[key])
-        }else{
-            objectToOverride = object[key] ;
-        }
+function checkCopyType(type){
+    if (type !== "deep" && type !== "shallow"){
+        throw new TypeError("wrong copy type");
+    }else{
+        return true ;
     }
 }
 
-export function deep_copy(object){
+export function override_object(object,objectToOverride,props_to_override = null,copy_type="deep"){
+    checkCopyType(copy_type);
+    if (props_to_override == null ){
+        for (let key in object){
+            objectToOverride[key] = copy(object[key],copy_type);
+        }
+    }else if (props_to_override instanceof Array){
+        for (let key of props_to_override){
+            objectToOverride[key] = copy(object[key],copy_type);
+        }
+    }else{
+        throw new TypeError("expected array in propos_to_override");
+    }
+}
+// type can be either "shallow" or "deep" ;
+function copy(object,props_to_copy,copy_type = "deep"){
+    checkCopyType(copy_type);
     let return_value ;
     if(typeof object === "object" ){
         return_value = new Object() ;
-        for (let key in object){
-            if (typeof object[key] === "object"){
-                return_value[key] = deep_copy(object[key]);
-            }else{
-                return_value[key] = object[key] ;
+        if (props_to_copy == null){
+            for (let key in object){
+                return_value[key] = (copy_type === "deep") ? deep_copy(object[key]): object[key];
             }
+        }else if(props_to_copy instanceof Array){
+            for (let key of props_to_copy){
+                return_value[key] = (copy_type === "deep") ? deep_copy(object[key]): object[key];
+            }
+        }else{
+            throw new TypeError("props_to_copy is not an Array !");
         }
     }else{
-        throw new TypeError("expected objet, got "+typeof object);
+        return_value = object ;
     }
     return return_value ;
+}
+
+export function deep_copy(object,props_to_copy = null){
+    return copy(object,props_to_copy,"deep");
+}
+
+export function shallow_copy(object,props_to_copy ){
+    return copy(object,props_to_copy,"shallow");
 }
