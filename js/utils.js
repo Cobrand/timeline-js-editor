30c1a8c034c1244a2dbaa129648e3fbfce799d4d
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2015-11-16 13:48:55 +0100

    Backbone.View -> react

--- js/utils.js
+++ js/utils.js
@@ -50,3 +50,38 @@ export function deep_copy(object,props_to_copy = null){
 export function shallow_copy(object,props_to_copy ){
     return copy(object,props_to_copy,"shallow");
 }
+
+
+// Use react instead of Backbone.View.
+// https://leoasis.github.io/posts/2014/03/22/from_backbone_views_to_react/
+React.Backbone = {
+    listenToProps: function(props) {
+        _.each(this.updateOnProps, function(events, propName) {
+            switch(events) {
+                case 'collection':
+                    events = 'add remove reset sort';
+                    break;
+                case 'model':
+                    events = 'change';
+            }
+            this.listenTo(props[propName], events, function() {
+                this.forceUpdate();
+            });
+        }, this);
+    },
+
+    componentDidMount: function() {
+        this.listenToProps(this.props);
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.stopListening();
+        this.listenToProps(nextProps);
+    },
+
+    componentWillUnmount: function() {
+        this.stopListening();
+    }
+}
+
+_.extend(React.Backbone, Backbone.Events);
