a9bdffb1f75183daa00c60d917ff5f19bcb360ed
Cobrand <cobrandw@gmail.com>
2015-11-16 22:10:18 +0100

    Added sass support

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import React from "react";
 import Backbone from "backbone";
+import "../scss/main.scss" ;
 import "model.js";
--- webpack.config.js
+++ webpack.config.js
@@ -25,13 +25,22 @@ module.exports = {
         //    },
         //],
         loaders: [{
-            test: /\.jsx?$/,
+            test: /\.jsx?$/, // compiles jsx+ES6 into browser compatible ES5
             exclude: /(node_modules)/,
             loader: 'babel-loader'
         },{
             test: /backbone\.js$/,
             loader: 'imports?define=>false'
-        }]
+        },
+        {
+            test: /\.(png|jpg)$/, // transforms require("imgpath") into urls
+            loader: 'url?limit=25000'
+        },
+        {
+            test: /\.s[a|c]ss$/, // must have scss to css compiler
+            loader: 'style!css!sass'
+        }
+    ]
     },
     plugins:  PROD ? [
         new webpack.optimize.UglifyJsPlugin({minimize:true}),
