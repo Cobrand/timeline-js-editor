a1b63520cc14e55d533781f3e200b563ba32854e
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2015-11-18 11:16:12 +0100

    Improve Slide render()

--- js/view.js
+++ js/view.js
@@ -8,13 +8,66 @@ export const Slide = React.createClass({
         "slide": "model",
     },
 
+    getDate() {
+        const s = this.props.slide;
+        if (s.display_date) {
+            return <p>{s.display_date}</p>;
+        } else if (s.end_date) {
+            return (<p> from {s.start_date.toString()}
+                       to {s.end_date && s.end_date.toString()} </p>);
+        } else {
+            return <p>{s.start_date.toString()}</p>;
+        }
+    },
+
+    getMedia() {
+        const s = this.props.slide;
+        if (!s.media) {
+            return;
+        }
+        const m = s.media;
+        const url = <a href="{m.url}">{m.url}</a>;
+        const caption = m.caption ?
+            <figcaption dangerouslySetInnerHTML={{__html: m.caption}}
+                        className="caption">
+            </figcaption> : null;
+        const credit = m.credit ?
+            <figcaption dangerouslySetInnerHTML={{__html: m.credit}}
+                        className="credit">
+            </figcaption> : null;
+        return (
+            <figure>
+                {url}
+                {caption}
+                {credit}
+            </figure>
+        );
+    },
+
+    getBg() {
+        const s = this.props.slide;
+        if (!s.background) {
+            return;
+        }
+        const b = s.background;
+        let style = Object.create(null);
+        if (b.url) {
+            style.backgroundImage = "url(" + b.url + ")";
+        }
+        if (b.color) {
+            style.backgroundColor = b.color;
+        }
+        return style;
+    },
+
     render() {
         const s = this.props.slide;
         return (
-            <div>
-                <h1>{s.text.headline}</h1>
-                <p>{s.text.text}</p>
-                <p>from {s.start_date} to {s.end_date}</p>
+            <div className="slide" style={this.getBg()}>
+                <h1 dangerouslySetInnerHTML={{__html: s.text.headline}} />
+                <p dangerouslySetInnerHTML={{__html: s.text.text}} />
+                {this.getDate()}
+                {this.getMedia()}
             </div>
         );
     }
