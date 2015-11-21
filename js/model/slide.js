bae30f8ac80cebb85d6823a4e27e181d26d78251
Cobrand <cobrandw@gmail.com>
2015-11-21 17:18:28 +0100

    [Nw] Enhanced slide.js code base

--- js/model/slide.js
+++ js/model/slide.js
@@ -1,4 +1,4 @@
-import {deep_override_object} from "utils.js" ;
+import {deep_override_object,uuid4} from "utils.js" ;
 import Backbone from "backbone";
 
 export const Slide = Backbone.Model.extend({
@@ -17,24 +17,23 @@ export const Slide = Backbone.Model.extend({
     toJSON() {
         const json = {};
 
-        if (this.start_date) {
-            json.start_date = this.start_date.toJSON();
+        let props_to_override_toJSON = ["start_date",
+                                        "end_date",
+                                        "text",
+                                        "media"];
+
+        for (let prop of props_to_override_toJSON){
+            if (this[prop] != null){
+                json[prop] = this[prop].toJSON();
+            }
         }
-        if (this.end_date) {
-            json.end_date = this.end_date.toJSON();
-        }
-        if (this.text) {
-            json.text = this.text.toJSON();
-        }
-        if (this.media) {
-            json.media = this.media.toJSON();
-        }
-        deep_ove
-        set_if(this, json, "group");
-        set_if(this, json, "display_date");
-        set_if(this, json, "background");
-        set_if(this, json, "autolink");
-        json.unique_id = this.unique_id;
+
+        let props_to_override = ["group",
+                                 "display_date",
+                                 "background",
+                                 "autolink",
+                                 "unique_id"];
+        deep_override_object(this,json,props_to_override)
 
         return json;
     },
@@ -53,7 +52,7 @@ export const Slide = Backbone.Model.extend({
             display_date: json.display_date,
             background: json.background,
             autolink: json.autolink,
-            unique_id: json.unique_id || utils.uuid4(),
+            unique_id: json.unique_id || uuid4(),
         };
     }
 });
