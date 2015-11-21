dc11447c42d0b4eb44c0e966263e51cefbc34ea2
Cobrand <cobrandw@gmail.com>
2015-11-21 17:00:02 +0100

    class Media is now in media.js

---
+++ js/model/media.js
@@ -0,0 +1,14 @@
+export class Media {
+    constructor(url, caption=null, credit=null, thumbnail=null){
+        this.url = url ;
+        this.caption = caption ;
+        this.credit = credit ;
+        this.thumbnail = thumbnail ;
+    }
+    static from_object(json) {
+        if (json == null) {
+            throw new TypeError("expected JSON object, got null");
+        }
+        return new Media(json.url, json.caption, json.credit, json.thumbnail);
+    }
+}
--- js/model/model.js
+++ js/model/model.js
@@ -3,22 +3,3 @@
  *
  * Matches https://timeline.knightlab.com/docs/json-format.html
  */
-
-import Backbone from "backbone";
-
-import * as utils from "utils.js";
-
-export class Media {
-    constructor(url, caption=null, credit=null, thumbnail=null){
-        this.url = url ;
-        this.caption = caption ;
-        this.credit = credit ;
-        this.thumbnail = thumbnail ;
-    }
-    static from_object(json) {
-        if (json == null) {
-            throw new TypeError("expected JSON object, got null");
-        }
-        return new Media(json.url, json.caption, json.credit, json.thumbnail);
-    }
-}
