43e83f70e148e40290c44c6715fe25319bd8d231
Cobrand <cobrandw@gmail.com>
2015-11-21 16:17:08 +0100

    [Not working] time.js from facto to classes

--- js/model/time.js
+++ js/model/time.js
@@ -1,30 +1,24 @@
-export const Text = {
-    from_object(json) {
-        if (!json) {
-            return null;
+import * from "utils.js" ;
+
+export class TextSlide {
+    constructor(headline,content){
+        this.headline = headline ;
+        this.content = content ;
+    }
+    static from_object(json) {
+        if (json == null) {
+            throw new TypeError("expected JSON object, got null");
         }
 
-        return make_Text(json.headline, json.text);
+        return new TextSlide(json.headline, json.text);
+    }
+    toJSON() {
+        return {
+            headline:this.headline,
+            text:this.content
+        }
     }
 };
-
-export function make_Text(headline=null, text=null) {
-    return {
-        __proto__: Text.prototype,
-
-        headline,
-        text,
-
-        toJSON() {
-            const json = {};
-
-            set_if(this, json, "headline");
-            set_if(this, json, "text");
-
-            return json;
-        }
-    };
-}
 
 const units = [
     "year",
@@ -36,8 +30,23 @@ const units = [
     "millisecond"
 ];
 
-export const MDate = {
-    from_object(json_false_month) {
+const date_formats = {
+    "year": "YYYY",
+    "month": "MMMM YYYY",
+    "day": "LL",
+    "hour": "LLL",
+    "minute": "LLL",
+    "second": "LL LTS",
+    "millisecond": "LL HH:mm:ss.SSS"
+};
+
+export class MDate {
+    constructor(moment_date,precision="day",display_date=null){
+        this.date = moment_date ;
+        this.precision = precision ;
+        this.display_date = display_date ;
+    }
+    static from_object(json_false_month) {
         if (!json_false_month) {
             return null;
         }
@@ -53,79 +62,46 @@ export const MDate = {
             precision = unit;
         }
 
-        return make_MDate(moment(json),
+        return new MDate(moment(json),
                           precision,
                           json.display_date);
     }
-}
+    toJSON() {
+        let obj = {"display_date": display_date};
 
-const date_formats = {
-    "year": "YYYY",
-    "month": "YYYY MMMM",
-    "day": "LL",
-    "hour": "LLL",
-    "minute": "LLL",
-    "second": "LL LTS",
-    "millisecond": "LL HH:mm:ss.SSS"
-};
-
-export function make_MDate(date, precision, display_date=null) {
-    return {
-        __proto__: MDate.prototype,
-
-        date,
-        precision,
-        display_date,
-
-        toString() {
-            return display_date || date.format(date_formats[precision]);
-        },
-
-        toJSON() {
-            let obj = {"display_date": display_date};
-
-            for (let unit in units) {
-                obj[unit] = date.get(unit);
-                if (json[unit] === precision) {
-                    break;
-                }
+        for (let unit in units) {
+            obj[unit] = date.get(unit);
+            if (json[unit] === precision) {
+                break;
             }
-            obj["month"] += 1;
-
-            return obj;
         }
-    };
+        obj["month"] += 1;
+
+        return obj;
+    }
 }
 
-export const Era = {
-    from_object(json) {
-        if (!json) {
-            return null;
+export class Era {
+    constructor(start_date,end_date,text=null){
+        this.start_date = start_date ;
+        this.end_date = end_date ;
+        this.text = text ;
+    }
+    static from_object(json) {
+        if (json == null) {
+            throw new TypeError("expected JSON object, got null");
         }
 
-        return make_Era(MDate.from_object(json.start_date),
-                        MDate.from_object(json.end_date),
-                        json.text);
+        const era = new Era(MDate.from_object(json.start_date),
+                            MDate.from_object(json.end_date),
+                            json.text)
+        return era ;
+    }
+    toJSON() {
+        return {
+            start_date: this.start_date.toJSON(),
+            end_date: this.end_date.toJSON(),
+            text: this.text,
+        };
     }
 };
-
-export function make_Era(start_date, end_date, text=null) {
-    return {
-        __proto__: Era.prototype,
-
-        start_date,
-        end_date,
-        text,
-
-        toJSON() {
-            const json = {
-                start_date: this.start_date.toJSON(),
-                end_date: this.end_date.toJSON()
-            };
-
-            set_if(this, json, "text");
-
-            return json;
-        }
-    };
-}
