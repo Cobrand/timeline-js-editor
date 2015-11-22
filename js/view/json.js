6b2e3c36be67e9dc3f39fd32b5c4c5e25f69b731
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2015-11-22 13:39:53 +0100

    Fix child type

--- js/view/json.js
+++ js/view/json.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "utils.js";
 
 export const Json = React.createClass({
     mixins: [React.Backbone],
 
     updateOnProps: {
-        "timeline": "collection",
+        "timeline": "model",
     },
 
     render() {
