f8c4092a69191e337930f8ef31fb778bde92a36e
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2015-12-11 16:29:35 +0100

    Placeholder 'show preview'

--- js/view/interface.js
+++ js/view/interface.js
@@ -28,6 +28,10 @@ export const Interface = React.createClass({
         });
     },
 
+    showPreview() {
+
+    },
+
     getSlide() {
         if (this.state.current_slide) {
             return <view.Slide slide={this.state.current_slide}/>;
@@ -38,15 +42,25 @@ export const Interface = React.createClass({
         const t = this.props.timeline;
         return (
             <div className="interface">
-                <button name="show_json"
-                        type="button"
-                        onClick={this.showJSON}>
-                    Export JSON
-                </button>
-                <view.Tabs tabs={t.get("events")}
-                           handleChangeTab={this.handleChangeTab}
-                           />
-                {this.getSlide()}
+                <div className="menu">
+                    <button name="show_json"
+                            type="button"
+                            onClick={this.showJSON}>
+                        Export JSON
+                    </button>
+                    <button name="preview"
+                            type="button"
+                            onClick={this.showPreview}>
+                        Apercu timeline
+                    </button>
+                </div>
+                <div className="content">
+                    <view.Tabs title={t.get("title")}
+                               tabs={t.get("events")}
+                               handleChangeTab={this.handleChangeTab}
+                               />
+                    {this.getSlide()}
+                </div>
                 {this.state.json}
             </div>
         );
