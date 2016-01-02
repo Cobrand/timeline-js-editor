a75dfffcbd3828bd022a8bb2720bc3f7378a584c
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-01-02 16:35:53 +0100

    Fix eslint warnings

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const PROD = process.argv.indexOf('--prod') !== -1 ||
-             process.argv.indexOf('--release') !== -1 ;
+const PROD = (process.argv.indexOf("--prod") !== -1 ||
+              process.argv.indexOf("--release") !== -1);
 
 module.exports = {
     entry: {
@@ -17,42 +17,42 @@ module.exports = {
         path: __dirname + "/static",
         filename: "js/app.js"
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     eslint: {
-        configFile: '.eslintrc'
+        configFile: ".eslintrc"
     },
     module: {
         preLoaders: [
             {
                 test: /\.js$/,
-                loader: "eslint-loader",
+                loader: "eslint",
                 exclude: /node_modules/
-            },
+            }
         ],
         loaders: [{
             test: /\.jsx?$/, // compiles jsx+ES6 into browser compatible ES5
             exclude: /(node_modules)/,
-            loader: 'babel-loader'
+            loader: "babel"
         },{
             test: /backbone\.js$/,
-            loader: 'imports?define=>false'
+            loader: "imports?define=>false"
         },
         {
             test: /\.json$/,
-            loader: 'json'
+            loader: "json"
         },
         {
             test: /\.(png|jpg)$/, // transforms require("imgpath") into urls
-            loader: 'url?limit=25000'
+            loader: "url?limit=25000"
         },
         {
             test: /\.s[a|c]ss$/, // must have scss to css compiler
             loader: ExtractTextPlugin.extract("css?sourceMap!sass?sourceMap")
         }
-    ]
+        ]
     },
     plugins:  (PROD ? [
-        new webpack.optimize.UglifyJsPlugin({minimize:true})
+        new webpack.optimize.UglifyJsPlugin({minimize: true})
     ] : [
     ]).concat([
         new webpack.IgnorePlugin(/^jquery$/),
