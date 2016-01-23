7a0081bdc2857ff939419f3ff3987b8e4006ea61
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-01-23 18:50:31 +0100

    Export all models

--- js/model/model.js
+++ js/model/model.js
@@ -6,9 +6,16 @@
 
 import {Timeline} from "model/timeline.js";
 import {Slide,Slides} from "model/slide.js";
+import {Media} from "model/structs/media.js";
+import {Text} from "model/structs/text.js";
+import {MDate,Era} from "model/structs/time.js";
 
 export default {
     Timeline,
     Slide,
-    Slides
+    Slides,
+    Media,
+    Text,
+    MDate,
+    Era
 };
