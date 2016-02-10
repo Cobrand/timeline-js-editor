908eb9e067d86936ff94027ec535d68c96839d1b
Andrès <andres.franco@etu.univ-nantes.fr>
2016-02-10 17:55:26 +0100

    Prettier 'no preview available'

--- js/embed_media.js
+++ js/embed_media.js
@@ -1,12 +1,6 @@
 import React from "react";
 
-function getOptimzedSize(size){
-    let optimized_size = {x:640,y:360} ;
-    return optimized_size ;
-}
-
-export function embed_media(link,size){
-    size = getOptimzedSize(size);
+export function embed_media(link){
     let match ;
     if (typeof link === "string"){
         if ( (match = link.match(/(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-]+)(&(amp;)?[\w\?=]*)?/)) != null ){
@@ -15,7 +9,7 @@ export function embed_media(link,size){
         }else if ((match = link.match(/(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpg|gif|jpeg|png))(?:\?([^#]*))?(?:#(.*))?/)) != null ){
             return (<img src={match[0]} alt="media preview" className="media_preview" />);
         }else{
-            return (<div></div>);
+            return (<div className="media_preview empty">Aucune prévisualisation disponible</div>);
         }
     }else{
         throw Error("Expected string, got "+typeof link+" instead");
