fe598593e59b3262d19f6813705aa36303c57459
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-02-10 12:34:06 +0100

    Add HTML export

--- js/view/json.js
+++ js/view/json.js
@@ -3,6 +3,33 @@ import Filesaver from "filesaver.js";
 import "utils.js";
 import model from "model/model.js";
 
+const EXPORT = `<div class="timeline">
+    <div id="timeline-embed-RANDOMID"></div>
+
+    <link title="timeline-styles"
+          rel="stylesheet"
+          href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css">
+    </link>
+    <style>
+        html, body, #timeline-embed {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+        }
+    </style>
+    <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js">
+    </script>
+    <script>
+        (function() {
+            "use strict";
+            var options = {};
+            window.timelinejs = new TL.Timeline("timeline-embed-RANDOMID",
+                                                TIMELINE,
+                                                options);
+        }());
+    </script>
+</div>`;
+
 export const Json = React.createClass({
     mixins: [React.Backbone],
 
@@ -16,6 +43,12 @@ export const Json = React.createClass({
         handleCloseJSON: React.PropTypes.func.isRequired
     },
 
+    getInitialState() {
+        return {
+            which_export: "json"
+        };
+    },
+
     saveAsFile(){
         // TODO set filename as title of timeline
         let filename = this.props.filename || "timeline" ;
@@ -24,10 +57,42 @@ export const Json = React.createClass({
         Filesaver.saveAs(blob, filename+".json");
     },
 
+    getEmbedHtml() {
+        const random_id = (Math.floor(Math.random() * 100000)).toString();
+        const timeline = JSON.stringify(this.props.timeline.toJSON());
+        return EXPORT.replace(/RANDOMID/g, random_id)
+                     .replace(/TIMELINE/, timeline);
+    },
+
+    onWhichExportChange(event) {
+        this.setState({
+            which_export: event.target.value
+        });
+    },
+
+    getExport() {
+        if (this.state.which_export == "json") {
+            return (
+                <textarea id="jsontextexport"
+                          className="textexport"
+                          readOnly="readOnly"
+                          value={JSON.stringify(this.props.timeline.toJSON())}>
+                </textarea>
+            );
+        } else {
+            return (
+                <textarea id="htmltextexport"
+                          className="textexport"
+                          readOnly="readOnly"
+                          value={this.getEmbedHtml()}>
+                </textarea>
+            );
+        }
+    },
+
     render() {
-        const t = this.props.timeline;
         return (
-            <div className="json">
+            <div className="export">
                 <button className="button main red fright"
                         name="close_json"
                         type="button"
@@ -40,9 +105,13 @@ export const Json = React.createClass({
                         onClick={this.saveAsFile}>
                     Sauvegarder en JSON
                 </button>
-                <h1>Exporter en json</h1>
-                <textarea id="jsontextexport" className="jsontextexport" readOnly="readOnly">{JSON.stringify(t.toJSON())}</textarea>
-
+                <h1>Exporter en</h1>
+                <select value={this.state.which_export}
+                        onChange={this.onWhichExportChange}>
+                    <option value="json">JSON</option>
+                    <option value="html">HTML</option>
+                </select>
+                {this.getExport()}
             </div>
         );
     }
