3ed27cb96a3962191b7e4a9a8d69819edabc07d2
Cobrand <cobrandw@gmail.com>
2016-02-21 16:17:38 +0100

    Modified server config file
    * .json -> .js to allow comments
    * server_config is not in tree anymore, onyl _example.js is
    * updated .gitignore accordingly
    * Added sequelize as a dependency

--- js/server/options.js
+++ js/server/options.js
@@ -2,17 +2,30 @@
 let fs = require("fs");
 let path = require("path")
 let options = {
-    port:8080
-} ;
-let config_filename = path.join(__dirname,"./../../","server_config.json") ;
-if (fs.statSync(config_filename).isFile()){
-    try {
-        Object.assign(options,require(config_filename) || {});
-    } catch (e) {
-        console.error("ERROR["+e.name+"] "+e.stack);
+    port:8080,
+    db:{
+        host:"localhost",
+        dialect:"mariadb",//'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql'
+        database:"database_name",
+        username:"username",
+        password:"password"
     }
-} else {
-    console.warn("WARNING: server_config.json file not found, taking default config")
+} ;
+let config_filename = path.join(__dirname,"./../../","server_config.js") ;
+try {
+    if (fs.statSync(config_filename).isFile()){
+        try {
+            Object.assign(options,require(config_filename) || {});
+        } catch (e) {
+            console.error("ERROR["+e.name+"] "+e.stack);
+        }
+    } else {
+        console.error("ERROR: server_config.js file not found");
+    }
+} catch (e) {
+    console.error("ERROR: server_config.js file not found");
+    console.error("please create one with your own config")
+    process.exit(1);
 }
 
 module.exports = options ;
---
+++ server_config_example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "port":8080, // default is 8080
+    "db":{
+        "host":"localhost",
+        "dialect":"mariadb",//'mysql'|'mariadb'|'sqlite'|'postgres'|'mssql'
+        "database":"database_name",
+        "username":"username",
+        "password":"password"
+    }
+}
