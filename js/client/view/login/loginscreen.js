6c524c0db8ac3677dbcddcc748d5ff7838521cf6
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-03-06 21:32:51 +0100

    Login screen pote

--- js/client/view/login/loginscreen.js
+++ js/client/view/login/loginscreen.js
@@ -8,9 +8,40 @@ export const LoginScreen = React.createClass({
         handleClose: React.PropTypes.func
     },
 
+    getInitialState() {
+        return {
+            id: "",
+            pass: ""
+        };
+    },
+
+    onChangeId(event) {
+        this.setState({id: event.target.value});
+    },
+
+    onChangePass(event) {
+        this.setState({pass: event.target.value});
+    },
+
+    onConnect() {
+
+    },
+
     render() {
         return (
             <div className="loginscreen">
+                <input type="text"
+                       placeholder="Identifiant"
+                       onChange={this.onChangeId}>
+                </input>
+                <input type="password"
+                       placeholder="Mot de passe"
+                       onChange={this.onChangePass}>
+                </input>
+                <button type="button"
+                        onClick={this.onConnect}>
+                    Connexion
+                </button>
                 <button className="button main red fright"
                         name="close_json"
                         type="button"
