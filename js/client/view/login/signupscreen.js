15ac08add084f5d78002b7f6baaeefcfbe497cba
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-03-06 21:49:19 +0100

    Signup pote

--- js/client/view/login/signupscreen.js
+++ js/client/view/login/signupscreen.js
@@ -8,9 +8,49 @@ export const SignUpScreen = React.createClass({
         handleClose: React.PropTypes.func
     },
 
+    getInitialState() {
+        return {
+            id: "",
+            pass: "",
+            email: ""
+        };
+    },
+
+    onChangeId(event) {
+        this.setState({id: event.target.value});
+    },
+
+    onChangePass(event) {
+        this.setState({pass: event.target.value});
+    },
+
+    onChangeEmail(event) {
+        this.setState({email: event.target.value});
+    },
+
+    onConnect() {
+
+    },
+
     render() {
         return (
             <div className="signupscreen">
+                <input type="text"
+                       placeholder="Identifiant"
+                       onChange={this.onChangeId}>
+                </input>
+                <input type="password"
+                       placeholder="Mot de passe"
+                       onChange={this.onChangePass}>
+                </input>
+                <input type="email"
+                       placeholder="email"
+                       onChange={this.onChangeEmail}>
+                </input>
+                <button type="button"
+                        onClick={this.onConnect}>
+                    Connexion
+                </button>
                 <button className="button main red fright"
                         name="close_json"
                         type="button"
