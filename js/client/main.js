35939052105f41443262c8155ff430aac08705db
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-03-30 21:33:14 +0200

    Indication de non sauvegarde si pas co.
    Closes #13.

--- js/client/main.js
+++ js/client/main.js
@@ -11,6 +11,15 @@ import "../../node_modules/sweetalert/dist/sweetalert.css";
 import model from "model/model.js";
 import view from "view/view.js";
 
+window.onbeforeunload = (event) => {
+    let msg = "Bien penser à sauvegarder !";
+
+    if (!window.localStorage.getItem("user_id")) {
+        (event || window.event).returnValue = msg;
+        return msg;
+    }
+};
+
 ReactDOM.render(<view.Interface login={new model.Login()}
                                 signup={new model.SignUp()}/>,
                 document.getElementById("root"));
